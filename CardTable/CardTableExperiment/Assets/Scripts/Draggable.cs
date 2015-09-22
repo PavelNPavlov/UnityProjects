fb5bf717a40e4555c3ec99fde55c1386e52b7d94
PavelNPavlov <pavelnapavlov@gmail.com>
2015-09-22 11:11:20 +0300

    Card Prefab and Draggable Script

---
+++ CardTable/CardTableExperiment/Assets/Scripts/Draggable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("DragEnd");
+    }
+}
